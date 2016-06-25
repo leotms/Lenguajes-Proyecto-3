5ce63c61304cc7b37758de208af1f33b430d582c
aldrix <aldrix_marfil@hotmail.com>
2016-06-25 15:53:31 -0400

    Definida estructura del arbol binario

--- trees.rb
+++ trees.rb
@@ -3,3 +3,31 @@
 # Autores:
 #     - Aldrix Marfil     #10-10940
 #     - Leonardo Martinez #11-10576
+
+#Implementación de la Clase Árbol Binario.
+class ArbolBinario
+	#include mod_bfs
+
+	attr_accessor :valor  #Valor almacenado en el nodo.
+	attr_reader   :hizq   #Referenca al hijo izquierdo.
+	attr_reader   :hder   #Referencia al hijo derecho.
+
+	#Inicialización de un árbol binario.
+	# valor: valor del nodo.
+	# hizq : hijo izquierdo.
+	# hder : hijo derecho.
+	def initialize(valor,hizq=nil,hder=nil)
+		@valor = valor
+		@hizq  = hizq
+		@hder  = hder
+
+	end
+
+
+	def each(bloq)
+		yield self.valor
+		self.hizq.each(&bloq) if self.hizq
+		self.hder.each(&bloq) if self.hder
+	end
+
+end
